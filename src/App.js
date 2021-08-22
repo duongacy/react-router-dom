029d499238ea98cb6625f2a0e82210e33e2d2c2d
duongy96@gmail.com <duongy96@gmail.com>
2021-08-22 11:19:23 +0700

    react router

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AuthExample from './pages/ReactRouterExample';
+
+function App() {
+    return (
+        <div className="App">
+            <AuthExample />
+        </div>
+    );
+}
+
+export default App;
